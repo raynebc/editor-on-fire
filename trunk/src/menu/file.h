175d532ccd2d2cfd3cc2f7ff0956e574c4628db1
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-05-16 00:35:35 +0000

    Fix for issue 76.  EOF will now run a controller key conflict test if a new guitar or drum key is defined.  If there is a conflict, an alert message is presented to the user.

--- trunk/src/menu/file.h
+++ trunk/src/menu/file.h
@@ -1,6 +1,8 @@
 #ifndef EOF_MENU_FILE_H
 #define EOF_MENU_FILE_H
 
+#include "../control.h"
+
 extern MENU eof_file_menu[];
 
 extern DIALOG eof_settings_dialog[];
@@ -53,5 +55,12 @@ void eof_lyric_import_prompt(int *selectedformat, char **selectedtrack);
 
 struct Lyric_Format *GetDetectionNumber(struct Lyric_Format *detectionlist, unsigned long number);
 	//Returns detection #number from the detection linked list
+
+int eof_test_controller_conflict(EOF_CONTROLLER *ct,int start,int stop);
+	//Tests for conflicts in the button[] array, from index #start to index #stop
+	//eof_guitar.button[0] and button[1] are used for guitar strum, button[2] through button[6] are used for the fret buttons
+	//eof_drums.button[0] is used for bass pedal, button[1] through button[4] are used for pads 1 through 4
+	//If there is a button conflict, nonzero is returned
+
 
 #endif
