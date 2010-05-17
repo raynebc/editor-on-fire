08e18a944baad86b52e0384d6a32e991c8fcb57a
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-05-17 22:55:59 +0000

    This is the untested beta version of my Feedback .chart import logic.  This is all theoretical and hasn't been tested.  I will test and make updates as necessary.
    When it is tested and working, we'll just need to add a function to translate the chart structure contents into EOF's format.

--- trunk/src/feedback.h
+++ trunk/src/feedback.h
@@ -3,4 +3,77 @@
 
 void eof_editor_logic_feedback(void);
 
+//A linked list storing anchors, each of which is a Set Tempo, Anchor or Time Signature event
+struct dBAnchor
+{
+	unsigned long chartpos;	//The defined chart position of the anchor
+	unsigned char type;	//The original event type in the imported file ('A' for anchor, 'B' for BPM change, 'T' for Time signature
+	unsigned long BPM;	//The tempo of this beat times 1000 (0 for no change from previous anchor)
+	unsigned char TS;	//The numerator of this beat's time signature (#/4) (0 for no change from previous anchor)
+	unsigned long usec;	//The real time position of this anchor in microseconds (millionths of a second) (0 if not an anchor)
+	struct dBAnchor *next;	//Pointer to the next anchor in the list
+};
+
+//A linked list strong text events
+struct dbText
+{
+	unsigned long chartpos;
+	char *text;
+	struct dbText *next;
+};
+
+//A linked list storing notes for an instrument track
+struct dbNotelist
+{
+	unsigned long chartpos;
+	unsigned char gemcolor;
+	unsigned long duration;
+	struct dbNotelist *next;
+};
+
+//A linked list of instrument tracks, each of which stores a linked list of notes
+struct dbTrack
+{
+	char *trackname;
+	struct dbNotelist *notes;
+	struct dbTrack *next;
+};
+
+//The structure representing the contents of a Feedback chart.  Relevant tags are stored, as are linked lists for all
+//anchors, text events and instrument tracks
+struct FeedbackChart
+{
+//Tags from the [Song] section
+	char *name;
+	char *artist;
+	char *charter;
+	unsigned long offset;
+	unsigned long resolution;	//Is expected to be 192.  If it's missing in the .char file, 192 could probably be assumed
+	unsigned long linesprocessed;	//The number of lines in the file that were processed
+	unsigned long tracksloaded;	//The number of instrument tracks that were loaded
+
+	struct dBAnchor *anchors;	//Linked list of anchors
+	struct dbText *events;		//Linked list of text events
+	struct dbTrack *tracks;		//Linked list of note tracks
+};
+
+int Read_dB_string(char *source,char **str1, char **str2);
+	//Scans the source string for a valid dB tag: text = text	or	text = "text"
+	//The text to the left of the equal sign is returned through str1 as a new string, with whitespace truncated
+	//The text to the right of the equal sign is returned through str2 as a new string, with whitespace truncated
+	//If the first non whitespace character encountered after the equal sign is a quotation mark, all characters after
+	//that quotation mark up to the next are returned through str2
+	//Nonzero is returned upon success, or zero is returned if source did not contain two sets of non whitespace characters
+	//separated by an equal sign character, or if the closing quotation mark is missing.
+
+char *Validate_dB_instrument(char *buffer);
+	//Validates that buffer contains a valid dB instrument track name enclosed in brackets []
+	//buffer is expected to point to the opening bracket
+	//If it is valid, a copy of the track name is returned, otherwise NULL is returned
+	//buffer[] is modified to remove any whitespace after the closing bracket
+
+void DestroyFeedbackChart(struct FeedbackChart *ptr, char freestruct);
+	//Releases all memory used by the passed FeedbackChart, including that of the contained linked lists
+	//If freestruct is nonzero, the passed FeedbackChart structure's memory is also freed
+
 #endif
