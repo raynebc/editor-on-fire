d02faeef5c1a737f3b5d96a560c378df0b45d79c
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-06-01 00:36:08 +0000

    Made changes to allow for ID3 tag import in EOF.  The biggest weakness in line-synchronized ID3 lyrics is that they have no defined length, so they are given a duration of 1ms.  Without making up a duration, there's no real solution for this.

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -2687,10 +2687,10 @@ int eof_initialize(int argc, char * argv[])
 	eof_filter_lyrics_files = ncdfs_filter_list_create();
 	if(!eof_filter_lyrics_files)
 	{
-		allegro_message("Could not create file list filter (*.txt, *.mid, *.lrc, *.vl, *.kar)!");
+		allegro_message("Could not create file list filter (*.txt, *.mid, *.lrc, *.vl, *.kar, *.mp3)!");
 		return 0;
 	}
-	ncdfs_filter_list_add(eof_filter_lyrics_files, "txt;mid;lrc;vl;kar", "Lyrics Files (*.txt, *.mid, *.lrc, *.vl, *.kar)", 1);
+	ncdfs_filter_list_add(eof_filter_lyrics_files, "txt;mid;lrc;vl;kar;mp3", "Lyrics Files (*.txt, *.mid, *.lrc, *.vl, *.kar, *.mp3)", 1);
 
 	eof_filter_exe_files = ncdfs_filter_list_create();
 	#ifdef ALLEGRO_WINDOWS
