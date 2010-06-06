4fb48a86378b1f5771753e431407694346f83dd4
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-06-06 02:54:37 +0000

    Added a custom playback rate option.  I haven't found how to make any rate that isn't a multiple of 25% to have smooth visuals during playback, but that probably deserves some attention.
    I also added hotkeys for Edit>Playback Rate and each of the selectable playback rates.

--- src/menu/edit.h
+++ src/menu/edit.h
@@ -77,6 +77,7 @@ int eof_menu_edit_playback_100(void);
 int eof_menu_edit_playback_75(void);
 int eof_menu_edit_playback_50(void);
 int eof_menu_edit_playback_25(void);
+int eof_menu_edit_playback_custom(void);	//Allow user to specify custom playback rate between 1 and 99 percent speed
 
 int eof_menu_edit_speed_slow(void);
 int eof_menu_edit_speed_medium(void);
