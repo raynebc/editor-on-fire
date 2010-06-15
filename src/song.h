f328bce61fe86e1e95a61e26a393e2e3f78a6e4c
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-06-15 04:18:39 +0000

    -Declared quicksort comparitor functions in song.h -Declared a EOF_MAX_BOOKMARK_ENTRIES macro and put it to use in song.h, song.c, editor.c, menu/song.c and note.c.  I left legacy.c alone just to make sure I didn't break compatibility with loading older versions of the .eof format -Corrected the behavior of eof_track_add_note() to return NULL if memory allocation failed.  Previously, it would return the address of the second to last successfully added note structure if allocation failed -Corrected the behavior of eof_vocal_track_add_lyric() to return NULL if memory allocation failed.  Previously, it would return the address of the second to last successfully added lyric structure if allocation failed -Added bounds checking in eof_track_add_star_power(), eof_track_add_solo(), eof_vocal_track_add_line(), eof_song_add_beat(), eof_song_add_text_event()

--- src/song.h
+++ src/song.h
@@ -10,6 +10,7 @@
 #define EOF_MAX_TRACKS            5
 #define EOF_MAX_CATALOG_ENTRIES 256
 #define EOF_MAX_INI_SETTINGS     32
+#define EOF_MAX_BOOKMARK_ENTRIES 10
 
 #define EOF_NOTE_SUPAEASY 0
 #define EOF_NOTE_EASY     1
@@ -50,18 +51,18 @@
 
 typedef struct
 {
-	
+
 	char          type;
 	char          note;
 	unsigned long pos;
 	long          length;
 	char          flags;
-	
+
 } EOF_NOTE;
 
 typedef struct
 {
-	
+
 	char          type;
 	char          note;
 	short         endbeat;
@@ -71,85 +72,85 @@ typedef struct
 	float         porpos;     // position of note within the beat (100.0 = full beat)
 	float         porendpos;
 	char          active;
-	
+
 } EOF_EXTENDED_NOTE;
 
 typedef struct
 {
-	
+
 	char          note;
 	char          text[256];
 	unsigned long pos;
 	long          length;
-	
+
 } EOF_LYRIC;
 
 typedef struct
 {
-	
+
 	char          note;
 	char          text[256];
 	unsigned long pos;
 	long          length;
-	
+
 	short         beat;       // which beat this note was copied from
 	short         endbeat;    // which beat this note was copied from
 	float         porpos;     // position of note within the beat (100.0 = full beat)
 	float         porendpos;
-	
+
 } EOF_EXTENDED_LYRIC;
 
 typedef struct
 {
-	
+
 	int start_pos;
 	int end_pos;
 	int flags;
-	
+
 } EOF_LYRIC_LINE;
 
 typedef struct
 {
-	
+
 	int start_pos;
 	int end_pos;
-	
+
 } EOF_SOLO_ENTRY;
 
 typedef struct
 {
-	
+
 	int start_pos;
 	int end_pos;
-	
+
 } EOF_STAR_POWER_ENTRY;
 
 typedef struct
 {
-	
+
 	EOF_NOTE * note[EOF_MAX_NOTES];
 	unsigned long notes;
-	
+
 	/* solos */
 	EOF_SOLO_ENTRY solo[EOF_MAX_SOLOS];
 	short solos;
-	
+
 	/* star power */
 	EOF_STAR_POWER_ENTRY star_power_path[EOF_MAX_STAR_POWER];
 	short star_power_paths;
-	
+
 } EOF_TRACK;
 
 typedef struct
 {
-	
+
 	EOF_LYRIC * lyric[EOF_MAX_LYRICS];
 	unsigned long lyrics;
-	
+
 	/* lyric lines */
 	EOF_LYRIC_LINE line[EOF_MAX_LYRIC_LINES];
 	short lines;
-	
+
 } EOF_VOCAL_TRACK;
 
 typedef struct
@@ -158,22 +159,22 @@ typedef struct
 	unsigned long ppqn;
 	unsigned long pos;
 	unsigned long flags;
-	
+
 	double fpos;
 
 } EOF_BEAT_MARKER;
 
 typedef struct
 {
-	
+
 	char filename[256];
 	int  midi_offset;
-	
+
 } EOF_OGG_INFO;
 
 typedef struct
 {
-	
+
 	char          artist[256];
 	char          title[256];
 	char          frettist[256];
@@ -181,104 +182,107 @@ typedef struct
 	char          loading_text[512];
 	char          lyrics;
 	char          eighth_note_hopo;
-	
+
 	EOF_OGG_INFO  ogg[8];
 	short         oggs;
-	
+
 	char          ini_setting[EOF_MAX_INI_SETTINGS][512];
 	short         ini_settings;
-	
+
 	unsigned long revision;
-	
+
 } EOF_SONG_TAGS;
 
 typedef struct
 {
-	
+
 	char track;
 	char type;
 	int start_pos;
 	int end_pos;
-	
+
 } EOF_CATALOG_ENTRY;
 
 typedef struct
 {
-	
+
 	char text[256];
 	int beat;
-	
+
 } EOF_TEXT_EVENT;
 
 typedef struct
 {
-	
+
 	EOF_CATALOG_ENTRY entry[EOF_MAX_CATALOG_ENTRIES];
 	int entries;
-	
+
 } EOF_CATALOG;
 
 typedef struct
 {
-	
+
 	/* song info */
 	EOF_SONG_TAGS * tags;
-	
+
 	/* track data */
 	EOF_TRACK       * track[EOF_MAX_TRACKS];
 	EOF_VOCAL_TRACK * vocal_track;
-	
+
 	EOF_BEAT_MARKER * beat[EOF_MAX_BEATS];
 	int beats;
-	
+
 	EOF_TEXT_EVENT * text_event[EOF_MAX_TEXT_EVENTS];
 	int text_events;
-	
+
 	/* miscellaneous */
-	int bookmark_pos[10];
+	int bookmark_pos[EOF_MAX_BOOKMARK_ENTRIES];
 	EOF_CATALOG * catalog;
-		
+
 } EOF_SONG;
 
-EOF_SONG * eof_create_song(void);
-void eof_destroy_song(EOF_SONG * sp);
-int eof_load_song_pf(EOF_SONG * sp, PACKFILE * fp);
-EOF_SONG * eof_load_song(const char * fn);
-int eof_save_song(EOF_SONG * sp, const char * fn);
+EOF_SONG * eof_create_song(void);	//Allocates, initializes and returns an EOF_SONG structure
+void eof_destroy_song(EOF_SONG * sp);	//De-allocates the memory used by the EOF_SONG structure
+int eof_load_song_pf(EOF_SONG * sp, PACKFILE * fp);	//Loads data from the specified PACKFILE pointer into the given EOF_SONG structure (called by eof_load_song())
+EOF_SONG * eof_load_song(const char * fn);	//Loads the specified EOF file, validating the file header and loading the appropriate OGG file
+int eof_save_song(EOF_SONG * sp, const char * fn);	//Saves the song to file
 
-EOF_NOTE * eof_track_add_note(EOF_TRACK * tp);
-void eof_track_delete_note(EOF_TRACK * tp, int note);
-void eof_track_sort_notes(EOF_TRACK * tp);
-int eof_fixup_next_note(EOF_TRACK * tp, int note);
-void eof_track_find_crazy_notes(EOF_TRACK * tp);
-void eof_track_fixup_notes(EOF_TRACK * tp, int sel);
-void eof_track_resize(EOF_TRACK * tp, int notes);
-void eof_track_add_star_power(EOF_TRACK * tp, unsigned long start_pos, unsigned long end_pos);
-void eof_track_delete_star_power(EOF_TRACK * tp, int index);
-void eof_track_add_solo(EOF_TRACK * tp, unsigned long start_pos, unsigned long end_pos);
-void eof_track_delete_solo(EOF_TRACK * tp, int index);
+EOF_NOTE * eof_track_add_note(EOF_TRACK * tp);	//Allocates, initializes and stores a new EOF_NOTE structure into the notes array.  Returns the newly allocated structure or NULL upon error
+void eof_track_delete_note(EOF_TRACK * tp, int note);	//Removes and frees the specified note from the notes array.  All notes after the deleted note are moved back in the array one position
+void eof_track_sort_notes(EOF_TRACK * tp);	//Performs a quicksort of the notes array
+int eof_song_qsort_notes(const void * e1, const void * e2);	//The comparitor function used to quicksort the notes array
+int eof_fixup_next_note(EOF_TRACK * tp, int note);	//Returns the note one after the specified note number that is in the same difficulty, or -1 if there is none
+void eof_track_find_crazy_notes(EOF_TRACK * tp);	//Used during MIDI import to mark a note as "crazy" if it overlaps with the next note in the same difficulty
+void eof_track_fixup_notes(EOF_TRACK * tp, int sel);	//Performs cleanup of the specified instrument track
+void eof_track_resize(EOF_TRACK * tp, int notes);	//Grows or shrinks the notes array to fit the specified number of notes by allocating/freeing EOF_NOTE structures
+void eof_track_add_star_power(EOF_TRACK * tp, unsigned long start_pos, unsigned long end_pos);	//Adds a star power phrase at the specified start and stop timestamp for the specified track
+void eof_track_delete_star_power(EOF_TRACK * tp, int index);	//Deletes the specified star power phrase and moves all phrases that follow back in the array one position
+void eof_track_add_solo(EOF_TRACK * tp, unsigned long start_pos, unsigned long end_pos);	//Adds a solo phrase at the specified start and stop timestamp for the specified track
+void eof_track_delete_solo(EOF_TRACK * tp, int index);	//Deletes the specified solo phrase and moves all phrases that follow back in the array one position
 
-EOF_LYRIC * eof_vocal_track_add_lyric(EOF_VOCAL_TRACK * tp);
-void eof_vocal_track_delete_lyric(EOF_VOCAL_TRACK * tp, int lyric);
-void eof_vocal_track_sort_lyrics(EOF_VOCAL_TRACK * tp);
-int eof_fixup_next_lyric(EOF_VOCAL_TRACK * tp, int lyric);
-void eof_vocal_track_fixup_lyrics(EOF_VOCAL_TRACK * tp, int sel);
-void eof_vocal_track_resize(EOF_VOCAL_TRACK * tp, int lyrics);
-void eof_vocal_track_add_line(EOF_VOCAL_TRACK * tp, unsigned long start_pos, unsigned long end_pos);
-void eof_vocal_track_delete_line(EOF_VOCAL_TRACK * tp, int index);
+EOF_LYRIC * eof_vocal_track_add_lyric(EOF_VOCAL_TRACK * tp);	//Allocates, initializes and stores a new EOF_LYRIC structure into the lyrics array.  Returns the newly allocated structure or NULL upon error
+void eof_vocal_track_delete_lyric(EOF_VOCAL_TRACK * tp, int lyric);	//Removes and frees the specified lyric from the lyrics array.  All lyrics after the deleted lyric are moved back in the array one position
+void eof_vocal_track_sort_lyrics(EOF_VOCAL_TRACK * tp);		//Performs a quicksort of the lyrics array
+int eof_song_qsort_lyrics(const void * e1, const void * e2);	//The comparitor function used to quicksort the lyrics array
+int eof_fixup_next_lyric(EOF_VOCAL_TRACK * tp, int lyric);	//Returns the next lyric, or -1 if there is none
+void eof_vocal_track_fixup_lyrics(EOF_VOCAL_TRACK * tp, int sel);	//Performs cleanup of the specified lyric track
+void eof_vocal_track_resize(EOF_VOCAL_TRACK * tp, int lyrics);	//Grows or shrinks the lyrics array to fit the specified number of notes by allocating/freeing EOF_LYRIC structures
+void eof_vocal_track_add_line(EOF_VOCAL_TRACK * tp, unsigned long start_pos, unsigned long end_pos);	//Adds a lyric phrase at the specified start and stop timestamp for the specified track
+void eof_vocal_track_delete_line(EOF_VOCAL_TRACK * tp, int index);	//Deletes the specified lyric phrase and moves all phrases that follow back in the array one position
 
-EOF_BEAT_MARKER * eof_song_add_beat(EOF_SONG * sp);
-void eof_song_delete_beat(EOF_SONG * sp, int beat);
-int eof_song_resize_beats(EOF_SONG * sp, int beats);
+EOF_BEAT_MARKER * eof_song_add_beat(EOF_SONG * sp);	//Allocates, initializes and stores a new EOF_BEAT_MARKER structure into the beats array.  Returns the newly allocated structure or NULL upon error
+void eof_song_delete_beat(EOF_SONG * sp, int beat);	//Removes and frees the specified beat from the beats array.  All beats after the deleted beat are moved back in the array one position
+int eof_song_resize_beats(EOF_SONG * sp, int beats);	//Grows or shrinks the beats array to fit the specified number of beats by allocating/freeing EOF_BEAT_MARKER structures
 
-void eof_song_add_text_event(EOF_SONG * sp, int beat, char * text);
-void eof_song_delete_text_event(EOF_SONG * sp, int event);
-void eof_song_move_text_events(EOF_SONG * sp, int beat, int offset);
-void eof_song_resize_text_events(EOF_SONG * sp, int events);
-void eof_sort_events(void);
+void eof_song_add_text_event(EOF_SONG * sp, int beat, char * text);	//Allocates, initializes and stores a new EOF_TEXT_EVENT structure into the text_event array.  Returns the newly allocated structure or NULL upon error
+void eof_song_delete_text_event(EOF_SONG * sp, int event);	//Removes and frees the specified text event from the text_events array.  All text events after the deleted text event are moved back in the array one position
+void eof_song_move_text_events(EOF_SONG * sp, int beat, int offset);	//Displaces all beats starting with the specified beat by the given additive offset.  Each affected beat's flags are cleared except for the anchor and text event(s) present flags
+void eof_song_resize_text_events(EOF_SONG * sp, int events);	//Grows or shrinks the text events array to fit the specified number of notes by allocating/freeing EOF_LYRIC structures
+void eof_sort_events(void);	//Performs a quicksort of the events array
+int eof_song_qsort_events(const void * e1, const void * e2);	//The comparitor function used to quicksort the events array
 
-void eof_sort_notes(void);
-void eof_fixup_notes(void);
-void eof_detect_difficulties(EOF_SONG * sp);
+void eof_sort_notes(void);	//Sorts the notes in all tracks
+void eof_fixup_notes(void);	//Performs cleanup of the note selection, beats and all tracks
+void eof_detect_difficulties(EOF_SONG * sp);	//Sets the populated status by prefixing each populated tracks' difficulty name (stored in eof_note_type_name[] and eof_vocal_tab_name[]) with an asterisk
 
 #endif
