08b72a685b2c71088523bd27e935ffba912d8a82
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-06-16 01:39:49 +0000

    Added bounds checking to eof_add_text_event()

--- trunk/src/event.h
+++ trunk/src/event.h
@@ -7,7 +7,7 @@ extern EOF_TEXT_EVENT eof_text_event[EOF_MAX_TEXT_EVENTS];
 extern char eof_event_list_text[EOF_MAX_TEXT_EVENTS][256];
 extern int eof_text_events;
 
-void eof_add_text_event(EOF_SONG * sp, int beat, char * text);
-void eof_move_text_events(EOF_SONG * sp, int beat, int offset);
+void eof_add_text_event(EOF_SONG * sp, int beat, char * text);	//Add the text event string as a new text event at the specified beat
+void eof_move_text_events(EOF_SONG * sp, int beat, int offset);	//Moves all text events defined at or after the specified beat ahead the specified number of beats (offset)
 
 #endif
