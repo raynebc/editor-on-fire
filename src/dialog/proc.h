b63671df34174a419e4e99710bae9ad9c26c2a52
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-07-22 21:37:56 +0000

    Improved the efficiency of eof_verified_edit_proc(), which will perform at most one parse of the permitted input character list per call.  It also prevents a crash in the event that the permitted list is NULL.

--- src/dialog/proc.h
+++ src/dialog/proc.h
@@ -4,5 +4,6 @@
 #include <allegro.h>
 
 int eof_verified_edit_proc(int msg, DIALOG *d, int c);
+	//Provides an input character validation string via d->dp2.  Any user inputted character not in the string is dropped (arrow keys, delete, backspace and escape are allowed)
 
 #endif
