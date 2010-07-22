4ae8ad31b70810b7dc9db01b963355c57cf2e4cf
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-07-22 21:37:56 +0000

    Improved the efficiency of eof_verified_edit_proc(), which will perform at most one parse of the permitted input character list per call.  It also prevents a crash in the event that the permitted list is NULL.

--- trunk/src/dialog/proc.c
+++ trunk/src/dialog/proc.c
@@ -7,40 +7,36 @@ int eof_verified_edit_proc(int msg, DIALOG *d, int c)
 	int i;
 	char * string = NULL;
 	int key_list[32] = {KEY_BACKSPACE, KEY_DEL, KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, KEY_ESC};
-	int always_allowed = 0;
-	
+	int match = 0;
+
 	if(msg == MSG_CHAR)
 	{
-		
 		for(i = 0; i < 7; i++)
 		{
-			if(c >> 8 == key_list[i])
+			if(c >> 8 == key_list[i])			//If the input is permanently allowed
 			{
-				always_allowed = 1;
-				break;
+				return d_agup_edit_proc(msg, d, c);	//Immediately allow the input character to be returned
 			}
 		}
-		
-		if(!always_allowed)
+
+		/* see if key is an allowed key */
+		string = (char *)(d->dp2);
+		if(string == NULL)	//If the accepted characters list is NULL for some reason
+			match = 1;	//Implicitly accept the input character instead of allowing a crash
+		else
 		{
-			/* see if key is an allowed key */
-			string = (char *)(d->dp2);
-			for(i = 0; i < strlen(string); i++)
+			for(i = 0; string[i] != '\0'; i++)	//Search all characters of the accepted characters list
 			{
 				if(string[i] == (c & 0xff))
 				{
+					match = 1;
 					break;
 				}
 			}
-			if(i < strlen(string))
-			{
-				return d_agup_edit_proc(msg, d, c);
-			}
-			else
-			{
-				return D_USED_CHAR;
-			}
 		}
+
+		if(!match)			//If there was no match
+			return D_USED_CHAR;	//Drop the character
 	}
-	return d_agup_edit_proc(msg, d, c);
+	return d_agup_edit_proc(msg, d, c);	//Allow the input character to be returned
 }
