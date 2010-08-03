6cebf088dd8d878cf977ac4908ed71c347c417a8
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-08-03 19:35:12 +0000

    Added to the ignore list and implemented a EOF_TIME_DIVISION macro.  This will make it easier to change the time division used during MIDI export, as well as make the realtime->delta conversion logic more understandable.

--- src/midi.h
+++ src/midi.h
@@ -14,9 +14,10 @@ typedef struct
 } EOF_MIDI_EVENT;
 
 #define EOF_MAX_MIDI_EVENTS 65536
+#define EOF_TIME_DIVISION 120.0
 
-int eof_figure_beat(double pos);				//Returns the beat marker immediately before the specified timestamp
-double eof_calculate_bpm_absolute(double pos);	//Returns the tempo defined by the beat marker immediately before the specified timestamp
+int eof_figure_beat(double pos);				//Returns the beat marker immediately before the specified timestamp, or -1 on failure
+double eof_calculate_bpm_absolute(double pos);	//Returns the tempo defined by the beat marker immediately before the specified timestamp, or 0.0 on failure
 double eof_calculate_delta(double start, double end);	//Finds the number of delta ticks within the specified time span?
 
 int eof_export_midi(EOF_SONG * sp, char * fn);	//Writes the specified chart's contents to the specified file
