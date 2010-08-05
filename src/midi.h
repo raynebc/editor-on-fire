18d414f10276067b0ef9c744c64fb9c44056a2ac
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-08-05 21:08:44 +0000

    Works on issue 123, implementing the new tempo map writing logic that writes all anchors using delta times that are multiples of EOF_TIME_DIVISION.  This should provide the maximum possible accuracy for the tempo map, and will ultimately allow for more accurate writing of the other tracks, which can now use their double floating point realtimes instead of an integer realtime value.

--- src/midi.h
+++ src/midi.h
@@ -2,6 +2,7 @@
 #define EOF_MIDI_H
 
 #include "song.h"
+#include "foflc/Midi_parse.h"
 
 typedef struct
 {
@@ -14,12 +15,24 @@ typedef struct
 } EOF_MIDI_EVENT;
 
 #define EOF_MAX_MIDI_EVENTS 65536
-#define EOF_TIME_DIVISION 120.0
+#define EOF_TIME_DIVISION 120
 
 int eof_figure_beat(double pos);				//Returns the beat marker immediately before the specified timestamp, or -1 on failure
 double eof_calculate_bpm_absolute(double pos);	//Returns the tempo defined by the beat marker immediately before the specified timestamp, or 0.0 on failure
 double eof_calculate_delta(double start, double end);	//Finds the number of delta ticks within the specified time span?
 
 int eof_export_midi(EOF_SONG * sp, char * fn);	//Writes the specified chart's contents to the specified file
+
+struct Tempo_change *eof_build_tempo_list(void);
+	//Parses eof_song->beat[], returning a linked list of anchors (tempo changes), or NULL on error
+	//Each link in the list gives the absolute delta and realtimes of the anchor
+
+struct Tempo_change *eof_add_to_tempo_list(unsigned long delta,double realtime,double BPM,struct Tempo_change *ptr);
+	//Creates and adds a new link EOF's tempo linked list referred to by ptr, and ptr is returned
+	//If ptr is NULL, a new list is created and the new head pointer is returned
+	//NULL is returned upon error
+
+void eof_destroy_tempo_list(struct Tempo_change *ptr);
+	//If ptr is not NULL, de-allocates all memory consumed by the tempo linked list
 
 #endif
