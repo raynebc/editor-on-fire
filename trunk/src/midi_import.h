e897c9d6fd0effa68471250366a008874603bb2c
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-08-06 22:59:42 +0000

    Works on issue 123.  The tempo map is generated first, so that all other delta to realtime conversions get to use floating point timestamps.  The import looks like it's extremely accurate now, but there's some lingering issue where notes incorrectly have "crazy" status applied to them.

--- trunk/src/midi_import.h
+++ trunk/src/midi_import.h
@@ -2,7 +2,12 @@
 #define EOF_MIDI_IMOPRT_H
 
 #include "song.h"
+#include "midi.h"
+#include "foflc/Midi_parse.h"
 
 EOF_SONG * eof_import_midi(const char * fn);
+EOF_SONG * eof_import_midi2(const char * fn);
+double eof_ConvertToRealTime(unsigned long absolutedelta,double starttime,struct Tempo_change *anchorlist,unsigned long timedivision);
+	//Converts and returns the converted realtime of the specified delta time based on the list of anchors and MIDI's time division
 
 #endif
