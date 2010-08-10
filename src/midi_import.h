040f5b0b4287342b269c1f82c0cd2e72d6e42101
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-08-10 21:03:50 +0000

    Defines a EOF_MAX_IMPORT_MIDI_TRACKS macro to make it easier to alter how many MIDI tracks EOF can import from a specified file.

--- src/midi_import.h
+++ src/midi_import.h
@@ -5,6 +5,8 @@
 #include "midi.h"
 #include "foflc/Midi_parse.h"
 
+#define EOF_MAX_IMPORT_MIDI_TRACKS 16
+
 EOF_SONG * eof_import_midi(const char * fn);
 EOF_SONG * eof_import_midi2(const char * fn);
 double eof_ConvertToRealTime(unsigned long absolutedelta,double starttime,struct Tempo_change *anchorlist,unsigned long timedivision);
