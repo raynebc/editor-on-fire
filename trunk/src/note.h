7ec9bd3db8a8cc12103a9c2b56f60dcfe5931ed3
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-09-03 22:25:08 +0000

    Removes a few hundred lines of code from eof_read_editor_keys() by reducing duplicated logic for adding new notes.

--- trunk/src/note.h
+++ trunk/src/note.h
@@ -11,6 +11,8 @@ void eof_note_create(EOF_NOTE * np, char g, char y, char r, char b, char p, int 
 	//Creates a note by by storing the specified on/off status of the green, yellow, red, blue and purple gem colors
 	//as well as the position and length into the given EOF_NOTE structure, whose contents are overwritten
 	//The note type and flags are not altered
+void eof_note_create2(EOF_NOTE * np, char note, unsigned long pos, long length);
+	//A simplified version of eof_note_create that accepts the fret status as a bitflag instead of individual characters
 //void eof_detect_notes(void);	//Undefined function
 int eof_adjust_notes(int offset);	//Applies the given additive offset to all notes, lyrics, bookmarks, catalog entries and solo/star power/lyric phrases
 //void eof_note_draw(EOF_NOTE * np, int p);	//Renders the note to the piano roll area
