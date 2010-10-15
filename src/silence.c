dee06613da11eaa23ea888e9c172dd843cf8501d
xander4jesus <xander4jesus@yahoo.com>
2010-10-15 02:07:08 +0000

    Add placeholder for silence adding function.

---
+++ src/silence.c
@@ -0,0 +1,6 @@
+#include <allegro.h>
+
+int eof_add_silence(const char * oggfn, unsigned long ms)
+{
+	return 0;
+}
---
+++ src/silence.h
@@ -0,0 +1,6 @@
+#ifndef EOF_SILENCE_H
+#define EOF_SILENCE_H
+
+int eof_add_silence(const char * oggfn, unsigned long ms);
+
+#endif
