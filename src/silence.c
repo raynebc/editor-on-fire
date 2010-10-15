5482fd1b2ae23c6014df0a673d5821bdf1a4f442
xander4jesus <xander4jesus@yahoo.com>
2010-10-15 21:44:25 +0000

    Silence adding function is now working. We just need to add a menu option and dialog for this feature to be complete.

--- src/silence.c
+++ src/silence.c
@@ -19,6 +19,7 @@ static SAMPLE * create_silence_sample(unsigned long ms)
 	int stereo;
 	int freq;
 	unsigned long samples;
+	int channels;
 	int i;
 	
 	if(eof_music_track)
@@ -27,19 +28,20 @@ static SAMPLE * create_silence_sample(unsigned long ms)
 		stereo = alogg_get_wave_is_stereo_ogg(eof_music_track);
 		freq = alogg_get_wave_freq_ogg(eof_music_track);
 		samples = msec_to_samples(ms);
+		channels = stereo ? 2 : 1;
 		sp = create_sample(bits, stereo, freq, samples);
 		if(sp)
 		{
 			if(bits == 8)
 			{
-				for(i = 0; i < samples; i++)
+				for(i = 0; i < samples * channels; i++)
 				{
 					((unsigned char *)(sp->data))[i] = 0x80;
 				}
 			}
 			else
 			{
-				for(i = 0; i < samples; i++)
+				for(i = 0; i < samples * channels; i++)
 				{
 					((unsigned short *)(sp->data))[i] = 0x8000;
 				}
@@ -49,11 +51,93 @@ static SAMPLE * create_silence_sample(unsigned long ms)
 	return sp;
 }
 
+/* saves a wave file to file pointer */
+static int save_wav_fp(SAMPLE * sp, PACKFILE * fp)
+{
+	size_t channels, bits, freq;
+	size_t data_size;
+	size_t samples;
+	size_t i, n;
+	int val;
+
+	channels = sp->stereo ? 2 : 1;
+	bits = sp->bits;
+	void * pval = NULL;
+
+	if(channels < 1 || channels > 2)
+	{
+		return 0;
+	}
+
+	samples = sp->len;
+	data_size = samples * channels * (bits / 8);
+	n = samples * channels;
+	
+	freq = sp->freq;
+
+	pack_fputs("RIFF", fp);
+	pack_iputl(36 + data_size, fp);
+	pack_fputs("WAVE", fp);
+
+	pack_fputs("fmt ", fp);
+	pack_iputl(16, fp);
+	pack_iputw(1, fp);
+	pack_iputw(channels, fp);
+	pack_iputl(freq, fp);
+	pack_iputl(samples * channels * (bits / 8), fp);
+	pack_iputw(channels * (bits / 8), fp);
+	pack_iputw(bits, fp);
+
+	pack_fputs("data", fp);
+	pack_iputl(data_size, fp);
+
+	if (bits == 8)
+	{
+		pval = sp->data;
+		pack_fwrite(pval, samples * channels, fp);
+	}
+	else if (bits == 16)
+	{
+		pval = sp->data;
+		unsigned short *data = pval;
+		for (i = 0; i < n; i++)
+		{
+			pack_iputw((short)(data[i] - 0x8000), fp);
+		}
+	}
+	else
+	{
+		TRACE("Unknown audio depth (%d) when saving wav ALLEGRO_FILE.\n", val);
+		return 0;
+	}
+
+	return 1;
+}
+
+
 /* fill this in with a WAV saving routine (Allegro does not supply one for some
  * reason */
 static int save_wav(const char * fn, SAMPLE * sp)
 {
-	return 0;
+    PACKFILE * file;
+
+    /* open file */
+    file = pack_fopen(fn, "w");
+    if(file == NULL)
+    {
+        return 0;
+    }
+
+    /* save WAV to the file */
+    if(!save_wav_fp(sp, file))
+    {
+        return 0;
+    }
+
+    /* close the file */
+    pack_fclose(file);
+
+    return 1;
 }
 
 int eof_add_silence(const char * oggfn, unsigned long ms)
@@ -70,14 +154,18 @@ int eof_add_silence(const char * oggfn, unsigned long ms)
 		
 		if(!save_wav(wavfn, sp))
 		{
-			printf("error saving wav\n");
+			printf("Error saving wav!\n");
+			destroy_sample(sp);
 			return 0;
 		}
 		destroy_sample(sp);
 		
 		/* back up original file */
 		sprintf(backupfn, "%s.backup", oggfn);
-		eof_copy_file((char *)oggfn, backupfn);
+		if(!exists(backupfn))
+		{
+			eof_copy_file((char *)oggfn, backupfn);
+		}
 		
 		/* encode the silence */
 		replace_filename(soggfn, eof_song_path, "silence.ogg", 1024);
