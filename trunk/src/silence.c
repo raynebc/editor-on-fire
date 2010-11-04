75015ade7b4a5ccb8d886393c40b255ed9dd327c
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-11-04 22:58:12 +0000

    Updates the proposed EOF project file format to allow sections to have names and works on issue 144 by adding a function that should insert silence by re-encoding the audio.

--- trunk/src/silence.c
+++ trunk/src/silence.c
@@ -23,7 +23,7 @@ static SAMPLE * create_silence_sample(unsigned long ms)
 	unsigned long samples;
 	int channels;
 	int i;
-	
+
 	if(eof_music_track)
 	{
 		bits = alogg_get_wave_bits_ogg(eof_music_track);
@@ -74,7 +74,7 @@ static int save_wav_fp(SAMPLE * sp, PACKFILE * fp)
 	samples = sp->len;
 	data_size = samples * channels * (bits / 8);
 	n = samples * channels;
-	
+
 	freq = sp->freq;
 
 	pack_fputs("RIFF", fp);
@@ -152,9 +152,9 @@ int eof_add_silence(const char * oggfn, unsigned long ms)
 	{
 		return 0;
 	}
-	
+
 	set_window_title("Adjusting Silence...");
-	
+
 	/* back up original file */
 	sprintf(backupfn, "%s.backup", oggfn);
 	if(!exists(backupfn))
@@ -162,7 +162,7 @@ int eof_add_silence(const char * oggfn, unsigned long ms)
 		eof_copy_file((char *)oggfn, backupfn);
 	}
 	delete_file(oggfn);
-	
+
 	/* encode the silence */
 /*	replace_filename(soggfn, eof_song_path, "silence.ogg", 1024);
 	sprintf(sys_command, "oggSilence -d %d -l%lu -o \"%s\"", alogg_get_bitrate_ogg(eof_music_track), ms, soggfn);
@@ -192,8 +192,8 @@ int eof_add_silence(const char * oggfn, unsigned long ms)
 		eof_fix_window_title();
 		return 0;
 	}
-	
-	
+
+
 	/* stitch the original file to the silence file */
 	sprintf(sys_command, "oggCat \"%s\" \"%s\" \"%s\"", oggfn, soggfn, backupfn);
 	if(system(sys_command))
@@ -202,9 +202,140 @@ int eof_add_silence(const char * oggfn, unsigned long ms)
 		eof_fix_window_title();
 		return 0;
 	}
-	
+
 	/* clean up */
 	delete_file(soggfn);
+	if(eof_load_ogg((char *)oggfn))
+	{
+		eof_fix_waveform_graph();
+		eof_fix_window_title();
+		return 1;
+	}
+	eof_fix_window_title();
+	return 0;
+}
+
+int eof_add_silence_recode(const char * oggfn, unsigned long ms)
+{
+	char sys_command[1024] = {0};
+	char backupfn[1024] = {0};
+	char wavfn[1024] = {0};
+	char soggfn[1024] = {0};
+	ALOGG_OGG *oggfile=NULL;
+	SAMPLE *decoded=NULL,*combined=NULL;
+	FILE *fp=NULL;
+	int bits;
+	int stereo;
+	int freq;
+	unsigned long samples;
+	int channels;
+	unsigned long ctr,index;
+
+	if(ms == 0)
+	{
+		return 0;
+	}
+
+	set_window_title("Adjusting Silence...");
+
+	/* back up original file */
+	sprintf(backupfn, "%s.backup", oggfn);
+	if(!exists(backupfn))
+	{
+		eof_copy_file((char *)oggfn, backupfn);
+	}
+	delete_file(oggfn);
+
+	/* Decode the OGG file into memory */
+	//Load OGG file into memory
+	fp=fopen(oggfn,"rb");
+	if(fp == NULL)
+		return 0;	//Return failure
+	oggfile=alogg_create_ogg_from_file(fp);
+	if(oggfile == NULL)
+	{
+		fclose(fp);
+		return 0;	//Return failure
+	}
+	//Decode OGG into memory
+	decoded=alogg_create_sample_from_ogg(oggfile);
+	if(decoded == NULL)
+	{
+		alogg_destroy_ogg(oggfile);
+		fclose(fp);
+		return 0;	//Return failure
+	}
+
+	/* Create a SAMPLE array large enough for the leading silence and the decoded OGG */
+	bits = alogg_get_wave_bits_ogg(oggfile);
+	stereo = alogg_get_wave_is_stereo_ogg(oggfile);
+	freq = alogg_get_wave_freq_ogg(oggfile);
+	alogg_destroy_ogg(oggfile);	//This is no longer needed
+	oggfile = NULL;
+	samples = msec_to_samples(ms);
+	channels = stereo ? 2 : 1;
+	combined = create_sample(bits,stereo,freq,samples+decoded->len);	//Create a sample array long enough for the silence and the OGG file
+	if(combined == NULL)
+	{
+		destroy_sample(decoded);
+		fclose(fp);
+		return 0;	//Return failure
+	}
+
+	/* Add the PCM data for the silence */
+	if(bits == 8)
+	{	//Create 8 bit PCM data
+		for(ctr=0,index=0;ctr < samples * channels;ctr++)
+		{
+			((unsigned char *)(combined->data))[index++] = 0x80;
+		}
+	}
+	else
+	{	//Create 16 bit PCM data
+		for(ctr=0,index=0;ctr < samples * channels;ctr++)
+		{
+			((unsigned short *)(combined->data))[index++] = 0x8000;
+		}
+	}
+
+	/* Add the decoded OGG PCM data*/
+	if(bits == 8)
+	{	//Copy 8 bit PCM data
+		for(ctr=0;ctr < decoded->len;ctr++)
+		{
+			((unsigned char *)(combined->data))[index++] = ((unsigned char *)(decoded->data))[ctr];
+		}
+	}
+	else
+	{	//Copy 16 bit PCM data
+		for(ctr=0;ctr < decoded->len;ctr++)
+		{
+			((unsigned short *)(combined->data))[index++] = ((unsigned short *)(decoded->data))[ctr];
+		}
+	}
+
+	/* encode the audio */
+	destroy_sample(decoded);	//This is no longer needed
+	replace_filename(wavfn, eof_song_path, "encode.wav", 1024);
+	save_wav(wavfn, combined);
+	destroy_sample(combined);	//This is no longer needed
+	replace_filename(soggfn, eof_song_path, "encode.ogg", 1024);
+	#ifdef ALLEGRO_WINDOWS
+		sprintf(sys_command, "oggenc2 -o \"%s\" -b %d \"%s\"", soggfn, alogg_get_bitrate_ogg(eof_music_track) / 1000, wavfn);
+	#else
+		sprintf(sys_command, "oggenc -o \"%s\" -b %d \"%s\"", soggfn, alogg_get_bitrate_ogg(eof_music_track) / 1000, wavfn);
+	#endif
+	if(system(sys_command))
+	{
+		eof_fix_window_title();
+		return 0;
+	}
+
+	/* replace the current OGG file with the new file */
+	eof_copy_file(soggfn, (char *)oggfn);	//Copy encode.ogg to the filename of the original OGG
+
+	/* clean up */
+	delete_file(soggfn);	//Delete encode.ogg
 	if(eof_load_ogg((char *)oggfn))
 	{
 		eof_fix_waveform_graph();
