83c89ac3b6a49060237f429bda1fb57fe33e64a5
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-11-07 08:30:33 +0000

    Works on issue 144 by adding logic to ensure that the chart is not modified if the user adds 0ms of silence and the original audio is already loaded.  A file compare routine has been added to test the loaded audio against the backup file.

--- src/utility.h
+++ src/utility.h
@@ -7,6 +7,7 @@ int eof_system(const char * command);
 
 void * eof_buffer_file(char * fn);	//Loads the specified file into a memory buffer and returns the buffer, or NULL upon error
 int eof_copy_file(char * src, char * dest);	//Copies the source file to the destination file.  Returns 0 upon error
+int eof_file_compare(char *file1, char *file2);	//Returns zero if the two files are the same
 
 int eof_check_string(char * tp);	//Returns nonzero if the string contains any non space ' ' characters and is at least one character long
 
