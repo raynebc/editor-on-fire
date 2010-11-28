a0e9af0287ebcf27fb736d3a6edbe97b8f21daef
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2010-11-28 03:44:27 +0000

    Fixes a crash in the paste from logic.

--- trunk/src/menu/edit.c
+++ trunk/src/menu/edit.c
@@ -2211,7 +2211,7 @@ int eof_menu_edit_paste_from_difficulty(unsigned long source_difficulty)
 		eof_prepare_undo(EOF_UNDO_TYPE_NOTE_SEL);
 		for(i = eof_song->legacy_track[tracknum]->notes; i > 0; i--)
 		{	//For each note in this instrument track, from last to first
-			if(eof_song->legacy_track[tracknum]->note[i]->type == eof_note_type)
+			if(eof_song->legacy_track[tracknum]->note[i-1]->type == eof_note_type)
 			{	//If this note is in the current difficulty
 				eof_track_delete_note(eof_song->legacy_track[tracknum], i - 1);	//Delete it
 			}
