568986aa58eb016c673a1ea49a7dfbdf6cc6dba0
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2011-04-02 23:49:14 +0000

    Adds a track definition for the 22 fret version of pro bass, which exists for some songs.

--- src/song.h
+++ src/song.h
@@ -337,7 +337,7 @@ typedef struct
 
 extern EOF_TRACK_ENTRY eof_default_tracks[EOF_TRACKS_MAX + 1];
 	//The list of default tracks that should be presented in EOF
-extern EOF_TRACK_ENTRY eof_midi_tracks[EOF_TRACKS_MAX + 11];
+extern EOF_TRACK_ENTRY eof_midi_tracks[EOF_TRACKS_MAX + 12];
 	//The list of MIDI track names pertaining to each instrument and harmony track
 
 typedef struct
