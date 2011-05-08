35ec515d84e74aa44b76f4eff4106f628be34fb1
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2011-05-08 09:35:24 +0000

    Updates the changelog and version string in preparation for another beta release.

--- trunk/src/main.h
+++ trunk/src/main.h
@@ -9,7 +9,7 @@
 #include "control.h"
 #include "editor.h"
 
-#define EOF_VERSION_STRING "EOF v1.8beta6"
+#define EOF_VERSION_STRING "EOF v1.8beta7"
 #define EOF_COPYRIGHT_STRING "(c)2008-2010 T^3 Software."
 
 #define KEY_EITHER_ALT (key[KEY_ALT] || key[KEY_ALTGR])
