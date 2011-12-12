a20aea3d4aa617470ff867ce4abfa6f2d5e5835a
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2011-12-12 07:06:59 +0000

    *Completed first draft of the Guitar Pro specification document.  I was able to parse a complete 1 track Guitar Pro 5.10 file I had previously acquired.  A parsing source program is being uploaded and will have its logic incorporated into EOF eventually. *Fixed a bug where BRE drum note tails were hidden when the "hide drum note tails" preference was enabled.

---
+++ src/gp_import.h
@@ -0,0 +1,33 @@
+#ifndef EOF_GP_IMPORT_H
+#define EOF_GP_IMPORT_H
+
+#ifndef EOF_BUILD
+	//Compile standalone parse utility, use macros to make the file compile without Allegro
+	#define PACKFILE FILE
+	#define pack_fread(x, y, z) fread(x, 1, y, z)
+	#define pack_fopen fopen
+	#define pack_fclose fclose
+	#define pack_getc getc
+	#define pack_fseek(x, y) fseek(x, y, SEEK_CUR)
+	#define EOF_SONG char
+	void eof_gp_debug_log(FILE *inf, char *text);		//Prints the current file position and the specified string to the console
+	char *eof_note_names[12] =	{"A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab"};		//The name of musical note 0 (0 semitones above A) to 11 (11 semitones above A)
+#else
+	void eof_gp_debug_log(PACKFILE *inf, char *text);	//Does nothing in the EOF build
+#endif
+
+void pack_ReadWORDLE(PACKFILE *inf, unsigned *data);
+	//Read a little endian format 2 byte integer from the specified file.  If data isn't NULL, the value is stored into it.
+void pack_ReadDWORDLE(PACKFILE *inf, unsigned long *data);
+	//Read a little endian format 4 byte integer from the specified file.  If data isn't NULL, the value is stored into it.
+int eof_read_gp_string(PACKFILE *inf, unsigned *length, char *buffer, char readfieldlength);
+	//Read a string, prefixed by a one-byte string length, from the specified file
+	//If readfieldlength is nonzero, a 4 byte field length that prefixes the string length is also read
+	//If length is not NULL, the string length is returned through it.
+	//Buffer must be able to store at least 256 bytes to avoid overflowing.
+void eof_gp_parse_bend(PACKFILE *inf);
+	//Parses the bend at the current file position, outputting debug logging appropriately
+
+#endif
+
+
