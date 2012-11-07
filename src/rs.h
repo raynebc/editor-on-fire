8f7a3e0c6c440145fbec1b54f8e0ee9ad67d6f7b
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2012-11-07 02:07:54 +0000

    *Fixed memory leaks related to the unique chord list handling. *Fixed a bug in RS export where the phrase iterations were written in milliseconds instead of seconds. *Added logic to RS export so that if a string in a chord is unused, the finger is properly indicated as -1, reducing the needed number of manual alterations to the XML file. *Added logic to RS export so that string muted individual notes are omitted, as Rocksmith doesn't support those. *Altered RS export so that single notes with a length of 1ms are exported with a sustain of 0. *Added logic to RS export so that if a section is defined in multiple places, each instance refers to the same phrase ID number.

--- src/rs.h
+++ src/rs.h
@@ -10,6 +10,12 @@ unsigned long eof_build_chord_list(EOF_SONG *sp, unsigned long track, unsigned l
 	//The function returns the number of unique chords contained in the list
 	//If there are no chords in the pro guitar track, or upon error, *results is set to NULL and 0 is returned
 
+unsigned long eof_build_section_list(EOF_SONG *sp, unsigned long **results);
+	//Parses the given chart, building a list of all unique section markers (case insensitive)
+	//Memory is allocated to *results to return the list, referring to each text event by number
+	//The function returns the number of unique sections contained in the list
+	//If there are no sections in the pro guitar track, or upon error, *results is set to NULL and 0 is returned
+
 int eof_export_rocksmith_track(EOF_SONG * sp, char * fn, unsigned long track);
 	//Writes the specified pro guitar track in Rocksmith's XML format
 	//fn is expected to point to an array at least 1024 bytes in size.  It's filename is altered to reflect the track's name (ie /PART REAL_GUITAR.xml)
