0252bd09295b200c982283c693449ae008257b94
raynebc <raynebc@1b2aedea-9eff-cdc0-263b-b87a75789507>
2015-07-08 03:05:09 +0000

    Fixed another RS export bug from the recent code de-duplication with handshape RS export where if an arrangement had no handshapes (ie. no chords) then an extra closing handShapes tag would be written.

--- trunk/src/rs.c
+++ trunk/src/rs.c
@@ -1396,7 +1396,10 @@ int eof_export_rocksmith_1_track(EOF_SONG * sp, char * fn, unsigned long track, 
 					}//If this note is in this difficulty and will export as a chord (at least two non ghosted/muted gems)
 				}//For each note in the track
 			}//On first pass, reset the handshape counter and count the number of handshapes.  On second pass, write handshapes.
-			(void) pack_fputs("      </handShapes>\n", fp);
+			if(handshapectr)
+			{	//If there were handshapes
+				(void) pack_fputs("      </handShapes>\n", fp);
+			}
 
 			//Write closing level tag
 			(void) pack_fputs("    </level>\n", fp);
@@ -2615,7 +2618,10 @@ int eof_export_rocksmith_2_track(EOF_SONG * sp, char * fn, unsigned long track, 
 					}//If this note is not ignored
 				}//For each note in the track
 			}//On first pass, count the number of handshapes.  On second pass, write handshapes.
-			(void) pack_fputs("      </handShapes>\n", fp);
+			if(handshapectr)
+			{	//If there were handshapes
+				(void) pack_fputs("      </handShapes>\n", fp);
+			}
 
 			//Write closing level tag
 			(void) pack_fputs("    </level>\n", fp);
