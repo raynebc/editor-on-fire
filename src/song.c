raynebc/editor-on-fire

commit b6b5ac1126c05683f0867ca45876875e74322f39

*Added a warning during save if any errors with a slide's end fret number are found. *Fixed a bug where shortening a note could allow it to result in a negative length.

src/song.c
@@ -4112,7 +4112,7 @@ void eof_track_sort_notes(EOF_SONG *sp, unsigned long track)
 		{	//For each note in the track, in reverse order
 			if(eof_selection.multi[ctr - 1])
 			{	//If this note is selected
-				tflags = eof_get_note_flags(sp, track, ctr - 1);
+				tflags = eof_get_note_tflags(sp, track, ctr - 1);
 				tflags |= EOF_NOTE_TFLAG_SORT;	//Set this temporary flag to track this note is selected
 				eof_set_note_tflags(sp, track, ctr - 1, tflags);	//Update the note flags
 			}
@@ -4669,20 +4669,20 @@ void eof_pro_guitar_track_fixup_notes(EOF_SONG *sp, unsigned long track, int sel
 
 			/* cleanup Rocksmith related slide/bend statuses */
 			if((tp->note[i-1]->slideend > tp->numfrets) || !(tp->note[i-1]->flags & EOF_PRO_GUITAR_NOTE_FLAG_RS_NOTATION))
-			{	//If the slide end position is invalid
+			{	//If the slide end position is invalid or not indicated to be defined
 				tp->note[i-1]->slideend = 0;	//Clear it
 			}
 			if(tp->note[i-1]->bendstrength && !(tp->note[i-1]->flags & EOF_PRO_GUITAR_NOTE_FLAG_RS_NOTATION))
-			{	//If the bend strength is invalid
+			{	//If the bend strength is invalid or not indicated to be defined
 				tp->note[i-1]->bendstrength = 0;	//Clear it
 			}
 			if((tp->note[i-1]->unpitchend > tp->numfrets) || !tp->note[i-1]->unpitchend)
-			{	//If the unpitched slide end position is invalid
+			{	//If the unpitched slide end position is invalid or not indicated to be defined
 				tp->note[i-1]->unpitchend = 0;	//Clear it
 				tp->note[i-1]->flags &= ~EOF_PRO_GUITAR_NOTE_FLAG_UNPITCH_SLIDE;	//Clear the related flag
 			}
 			if(!tp->note[i-1]->slideend && !tp->note[i-1]->bendstrength)
-			{	//If this note has no slide end fret number or bend strength defined
+			{	//If this note has no slide end fret number or bend strength defined, or neither are indicated to be defined
 				if(!((tp->note == tp->technote) && (tp->note[i-1]->flags & EOF_PRO_GUITAR_NOTE_FLAG_BEND)))
 				{	//If tech view is in effect, a bend note is allowed to have a bend strength of 0
 					tp->note[i-1]->flags &= ~EOF_PRO_GUITAR_NOTE_FLAG_RS_NOTATION;	//Otherwise both the slide end position and the bend strength are undefined, clear this flag
@@ -6333,7 +6333,10 @@ void eof_adjust_note_length(EOF_SONG * sp, unsigned long track, unsigned long am
 				}
 				if(dir < 0)
 				{	//If the tail is being shortened by one grid snap
-					eof_note_set_tail_pos(sp, eof_selected_track, i, eof_tail_snap.previous_snap);
+					if(eof_tail_snap.previous_snap > notepos)
+					{	//Only allow the tail to move if it will still be at least 1ms after the start of the note
+						eof_note_set_tail_pos(sp, eof_selected_track, i, eof_tail_snap.previous_snap);
+					}
 				}
 				else
 				{	//If the tail is being lengthened by one grid snap
@@ -6358,8 +6361,12 @@ void eof_adjust_note_length(EOF_SONG * sp, unsigned long track, unsigned long am
 						eof_note_set_tail_pos(sp, eof_selected_track, i, eof_tail_snap.previous_snap);
 					}
 				}
+			}//If adjusting by the current grid snap value
+			if(eof_get_note_length(sp, track, i) <= 0)
+			{	//If the note's length became less than 1 for any reason
+				eof_set_note_length(sp, track, i, 1);	//Set it to 1ms
 			}
-		}
+		}//If the note is selected and in the active instrument difficulty
 	}//For each note in the track
 	eof_track_fixup_notes(sp, eof_selected_track, 1);
 	if(note_selection_updated)

src/main.c
@@ -2840,10 +2840,10 @@ void eof_render_note_window(void)
 			{	//If a note is selected
 				textprintf_ex(eof_window_note->screen, font, 2, ypos, eof_color_white, -1, "Note = %ld : Pos = %lu : Length = %lu", eof_selection.current, eof_get_note_pos(eof_song, eof_selected_track, eof_selection.current), eof_get_note_length(eof_song, eof_selected_track, eof_selection.current));
 ///Keep for debugging
-//#ifdef EOF_DEBUG
-//				ypos += 12;
-//				textprintf_ex(eof_window_note->screen, font, 2, ypos, eof_color_white, -1, "#Mask=%ld : Flags=%lX : Eflags=%X : Tflags=%X", eof_get_note_note(eof_song, eof_selected_track, eof_selection.current), eof_get_note_flags(eof_song, eof_selected_track, eof_selection.current), eof_get_note_eflags(eof_song, eof_selected_track, eof_selection.current), eof_get_note_tflags(eof_song, eof_selected_track, eof_selection.current));
-//#endif
+#ifdef EOF_DEBUG
+				ypos += 12;
+				textprintf_ex(eof_window_note->screen, font, 2, ypos, eof_color_white, -1, "#Mask=%ld : Flags=%lX : Eflags=%X : Tflags=%X", eof_get_note_note(eof_song, eof_selected_track, eof_selection.current), eof_get_note_flags(eof_song, eof_selected_track, eof_selection.current), eof_get_note_eflags(eof_song, eof_selected_track, eof_selection.current), eof_get_note_tflags(eof_song, eof_selected_track, eof_selection.current));
+#endif
 			}
 			else
 			{

src/main.h
@@ -9,7 +9,7 @@
 #include "control.h"
 #include "editor.h"
 
-#define EOF_VERSION_STRING "EOF v1.8RC10"
+#define EOF_VERSION_STRING "EOF v1.8RC10 (9-15-2015)"
 #define EOF_COPYRIGHT_STRING "(c)2008-2010 T^3 Software."
 
 #define KEY_EITHER_ALT (key[KEY_ALT] || key[KEY_ALTGR])

src/menu/file.c
@@ -2422,7 +2422,7 @@ int eof_save_helper(char *destfilename, char silent)
 	char function;		//Will be set to 1 for "Save" or 2 for "Save as"
 	int jumpcode = 0;
 	char fixvoxpitches = 0, fixvoxphrases = 0;
-	char note_length_warned = 0, note_distance_warned = 0, arpeggio_warned = 0, slide_warned = 0, bend_warned = 0;
+	char note_length_warned = 0, note_distance_warned = 0, arpeggio_warned = 0, slide_warned = 0, bend_warned = 0, slide_error = 0;
 	time_t seconds;		//Will store the current time in seconds
 	struct tm *caltime;	//Will store the current time in calendar format
 	unsigned short user_warned = 0;	//Tracks whether the user was warned about hand positions being undefined and auto-generated during Rocksmith and Bandfuse exports
@@ -2693,7 +2693,7 @@ int eof_save_helper(char *destfilename, char silent)
 		}//For each track, or until the user is warned about an offending arpeggio
 	}
 
-	/* check if any slide notes don't define their end position or bend notes don't define their bend strength */
+	/* check if any slide notes don't validly define their end position or bend notes don't define their bend strength */
 	if(!silent)
 	{	//If checks and warnings aren't suppressed
 		if(eof_write_rs_files || eof_write_rs2_files || eof_write_bf_files)
@@ -2733,8 +2733,46 @@ int eof_save_helper(char *destfilename, char silent)
 								bend_warned = 1;
 							}
 						}
-						if(slide_warned && bend_warned)
-							break;	//Exit for loop if both warnings were issued and the user declined both
+
+						if(!slide_error)
+						{	//If the user hasn't been warned about any slide related errors yet
+							unsigned char lowestfret = eof_pro_guitar_note_lowest_fret(tp, ctr2);	//Determine the note's lowest used fret value
+
+							if(flags & EOF_PRO_GUITAR_NOTE_FLAG_RS_NOTATION)
+							{	//If the note has pitched slide end position data
+								if(flags & EOF_PRO_GUITAR_NOTE_FLAG_SLIDE_UP)
+								{	//If the note slides up
+									if(tp->note[ctr2]->slideend <= lowestfret)
+									{	//The slide doesn't go higher than the current note
+										slide_error = 1;
+									}
+								}
+								if(flags & EOF_PRO_GUITAR_NOTE_FLAG_SLIDE_DOWN)
+								{	//If the note slides down
+									if(tp->note[ctr2]->slideend >= lowestfret)
+									{	//The slide doesn't go lower than the current note
+										slide_error = 1;
+									}
+								}
+							}
+							if(flags & EOF_PRO_GUITAR_NOTE_FLAG_UNPITCH_SLIDE)
+							{	//If the note has an unpitched slide
+								if(tp->note[ctr2]->unpitchend == lowestfret)
+								{	//The slide doesn't move from the current note
+									slide_error = 1;
+								}
+							}
+							if(slide_error)
+							{	//If one of the above checks failed
+								eof_seek_and_render_position(ctr, tp->note[ctr2]->type, tp->note[ctr2]->pos);
+								if(alert("Warning:  At least one slide note has an error in its end position.", "Unless corrected, it will not export to XML as a slide.", "Cancel save?", "&Yes", "&No", 'y', 'n') == 1)
+								{	//If the user opts to cancel
+									return 1;	//Return cancellation
+								}
+							}
+						}
+						if(slide_warned && bend_warned && slide_error)
+							break;	//Exit for loop if all warnings/errors were issued and declined by the user
 					}
 				}
 			}

commit e9f5fb8b5eb72e805717330225817c4b6deb9076

Fixed a bug introduced in the b6b5ac1126c05683f0867ca45876875e74322f39 commit that prevents shortening notes by a grid snap value from working correctly.  Shortening by grid snap also will no longer mark the chart as modified if none of the selected notes are longer than 1ms.

src/song.c
@@ -6447,20 +6447,35 @@ void eof_adjust_note_length(EOF_SONG * sp, unsigned long track, unsigned long am
 					targetpos--;	//Consider the tail's current position to end in the previous beat so that the grid snap logic will find it a grid snap position in that beat
 				}
 				eof_snap_logic(&eof_tail_snap, targetpos);	//Find grid snap positions before and after the tail's current ending position
-				if(!undo_made)
-				{	//Ensure an undo state was made before increasing the length
-					eof_prepare_undo(EOF_UNDO_TYPE_NOTE_LENGTH);
-					undo_made = 1;
-				}
 				if(dir < 0)
 				{	//If the tail is being shortened by one grid snap
+					unsigned long newtailendpos;
+
 					if(eof_tail_snap.previous_snap > notepos)
-					{	//Only allow the tail to move if it will still be at least 1ms after the start of the note
-						eof_note_set_tail_pos(sp, eof_selected_track, i, eof_tail_snap.previous_snap);
+					{	//Only allow the tail to shorten to end at the previous grid snap if it will still end after the note begins
+						newtailendpos = eof_tail_snap.previous_snap;
+					}
+					else
+					{	//Otherwise enforce the minimum length of 1ms
+						newtailendpos = notepos + 1;
+					}
+					if(newtailendpos != notepos + notelength)
+					{	//If the note length is actually changing
+						if(!undo_made)
+						{	//Ensure an undo state was made before increasing the length
+							eof_prepare_undo(EOF_UNDO_TYPE_NOTE_LENGTH);
+							undo_made = 1;
+						}
+						eof_note_set_tail_pos(sp, eof_selected_track, i, newtailendpos);
 					}
 				}
 				else
 				{	//If the tail is being lengthened by one grid snap
+					if(!undo_made)
+					{	//Ensure an undo state was made before increasing the length
+						eof_prepare_undo(EOF_UNDO_TYPE_NOTE_LENGTH);
+						undo_made = 1;
+					}
 					eof_note_set_tail_pos(sp, eof_selected_track, i, eof_tail_snap.next_snap);
 				}
 				newnotelength = eof_get_note_length(sp, eof_selected_track, i);